2a71f1062825b6521abac654c346c363df9e1e82
Jerzy Kamiński <86363785+jrzkaminski@users.noreply.github.com>
2024-06-15 20:56:07 +0300

    Replaced 4 turbo with 4o

--- core/artwork_analysis.py
+++ core/artwork_analysis.py
@@ -57,7 +57,7 @@ class ArtworkAnalyser:
         ]
         print('Images: ', images)
         response = self.client.chat.completions.create(
-            model="gpt-4-turbo",
+            model="gpt-4o",
             messages=[
                 {
                     "role": "user",
